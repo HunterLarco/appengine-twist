c99a3a6f6ac238a52641cdbe5e369547a6927b91
Hunter John Larco <admin@larcolabs.com>
2016-02-02 12:53:45 -0800

    Idea to use polymorphism to implement twist protocols

--- server/app.py
+++ server/app.py
@@ -1,20 +1,67 @@
 import twist
 
 
-api = twist.api(debug=True)
+class Protocol(object):
+  def __init__(self, funct):
+    self.__funct = funct
+  
+  def __call__(self, *args, **kwargs):
+    print('test')
 
 
-api.post('/api/login/?')
+class JsonProtocol(twist.Protocol):
+  def read(self):
+    from json import loads
+    try:
+      self.request.json = loads(self.request.body)
+    except:
+      self.request.json = None
+  
+  def write(self, returned):
+    from json import dumps
+    self.response.headers('Content-Type', 'application/json')
+    self.response.write(dumps(returned, indent=2))
+
+
+class ErrorProtocol(twist.Protocol):
+  def __init__(self, errormap):
+    self.errors = errormap
+  
+  def execute(self, funct):
+    try:
+      return funct(self.request, self.response)
+    except Exception as error:
+      if not error in self.errors:
+        return {'success':False, 'code':-1, 'message':'Unknown Server Error'}
+      else:
+        errorTuple = self.errors[error]
+        return {'success':False, 'code':errorTuple[0], 'message':errorTuple[1]}
+  
+  def write(self, returned):
+    if not 'success' in returned:
+      returned['success'] = True
+    return returned
+
+
+api = twist.api(debug=True, protocol=(JsonProtocol, ErrorProtocol))
+
+
+@api.post('/api/login/?')
 def LoginUser(request, response):
-  json = request.json()
+  url = request.url()
+  key = url.parametes('key')
+  
+  json = request.json
   userid, postid = json.parameters('userid', 'postid')
   Link.vote(userid, postid)
-  return {
-    'success': True
-  }
+  
+  response.headers['Content-Type'] = 'application/json'
+  response.headers('Content-Type', 'application/json')
+  
+  return { 'success': True }
 
 
-api.get('/.*')
+@api.get('/.*')
 def MainHandler(request, response):
   response.out.template('main', posts=Post.queryAll())
 
